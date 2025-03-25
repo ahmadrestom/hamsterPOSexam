873641f3dedcb90dd33cfb27606172f0f37a79ce
Ahmad Restom <ahmadrestom11@gmail.com>
2025-03-25 19:27:55 +0200

    Register Page

--- Frontend/hamster-pos/src/app/page.tsx
+++ Frontend/hamster-pos/src/app/page.tsx
@@ -1,7 +1,14 @@
-import Image from "next/image";
+"use client"; // Required for client-side hooks like useRouter
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <p>Ahmad</p>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/register"); // Redirect to the register page
+  }, []);
+
+  return null; // No content because user is redirected
 }
